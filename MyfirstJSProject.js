56b96aceebdb67a339300bccffecb7e09f755b4b
Nguyễn Quốc Hưng <87801986+thecalibur@users.noreply.github.com>
2021-08-10 16:25:02 +0700

    Create MyfirstJSProject.js

---
+++ MyfirstJSProject.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <!-- 
+        Các loại thẻ thông dụng:
+        h1 - h6 (heading)
+        p (paragraph)
+        img (image)
+
+     -->
+
+
+    <!-- Phần đầu -->
+    <head>
+        <title>Billy Shop</title>
+        <meta charset ="utf-8">
+    </head>
+
+    <!-- Phần thân -->
+    <body>
+        <h1>Hello Bill</h1>
+        <p>Bill is a man who loves programming</p>
+        <img src="https://cdn-images-1.medium.com/max/2000/1*j8mUsMxSnyJMix7aFiwLeA.png" alt="Ảnh minh họa">
+    </body>
+
+</html>
